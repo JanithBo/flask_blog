32734e80a3f80e3571bbaeb7dbc5c20836df8a2c
JanithBo <72045158+JanithBo@users.noreply.github.com>
2023-11-30 15:16:54 +0000

    code for modifying the post

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
 
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
+
 
 def get_db_connection():
   conn = sqlite3.connect('database.db')
@@ -25,10 +21,21 @@ def get_post(post_id):
          abort(404)
     return post
 
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     return render_template('post.html', post=post)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    return render_template('create.html')
 
 if __name__=='__main__':
   app.run(host='0.0.0.0',debug=True)
