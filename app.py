d98a249491f2a47853de17c654f607a9d25a7a32
JanithBo <72045158+JanithBo@users.noreply.github.com>
2023-11-30 15:23:04 +0000

    code for deleting the post

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ def create():
           return redirect(url_for('index'))
     return render_template('create.html')
 
-. . .
 
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
@@ -72,6 +71,16 @@ def edit(id):
             return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_post(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(post['title']))
+    return redirect(url_for('index'))
 
 if __name__=='__main__':
   app.run(host='0.0.0.0',debug=True)
